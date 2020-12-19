a826a6614174336a3a8dad6dcc40f24d35ff23be
Brendan <b@infosecb.com>
2020-12-19 08:22:06 -0500

    added test

--- tests/test_ipify_me.py
+++ tests/test_ipify_me.py
@@ -4,11 +4,12 @@
 
 
 import unittest
+import re
 from click.testing import CliRunner
 
 # from ipify_me import ipify_me
 from ipify_me import cli
-
+from ipify_me import core
 
 class TestIpify_me(unittest.TestCase):
     """Tests for `ipify_me` package."""
@@ -20,7 +21,9 @@ class TestIpify_me(unittest.TestCase):
         """Tear down test fixtures, if any."""
 
     def test_000_something(self):
-        """Test something."""
+        """Test the Get IP function."""
+        result = core.get_ipify_ip()
+        assert re.search('\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b', result) == True
 
     def test_command_line_interface(self):
         """Test the CLI."""
