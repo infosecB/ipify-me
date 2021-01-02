6df0f6283e6d92e1405f08cea0119765bd188dc9
Brendan <b@infosecb.com>
2021-01-02 10:33:03 -0500

    Bump version: 1.0.2 → 1.1.0

--- ipify_me/__init__.py
+++ ipify_me/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Brendan Chamberlain"""
 __email__ = 'brendan@infosecb.com'
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 from .core import get_ipify_ip
 from .core import print_ipify_ip
